5e1aea5e4f499adc559f9a18e779d94da23907cf
Choudhury Iqbal <ChoudhuryIqbal@users.noreply.github.com>
2025-04-20 02:45:21 -0400

    Update OAuthTest.java

--- src/test/java/api/test/OAuthTest.java
+++ src/test/java/api/test/OAuthTest.java
@@ -17,14 +17,13 @@ public class OAuthTest {
 				.header("Content-Type", "application/x-www-form-urlencoded")
 				.formParam("grant_type", "client_credentials").formParam("client_id", clientId)
 				.formParam("client_secret", clientSecret).when().post().then().statusCode(200).extract().response();
-	System.out.println(	tokenResponse.prettyPrint());
-		//String accessToken = tokenResponse.path("access_token");
-		String accessToken =tokenResponse.jsonPath().getString("access_token");
-System.out.println("Token is "+accessToken);
+		System.out.println(tokenResponse.prettyPrint());
+		// String accessToken = tokenResponse.path("access_token");
+		String accessToken = tokenResponse.jsonPath().getString("access_token");
+		System.out.println("Token is " + accessToken);
 		// Step 2: Use token to fetch animal info
 		given().baseUri("https://api.petfinder.com").basePath("/v2/animals/76093542")
-				.header("Authorization", "Bearer " + accessToken).when().get().then().statusCode(200).log()
-				.all();
-				
+				.header("Authorization", "Bearer " + accessToken).when().get().then().statusCode(200).log().all();
+
 	}
 }
